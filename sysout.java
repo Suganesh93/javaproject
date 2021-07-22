dd291317a930801c9efb793aaf2d6b8cd707ddce
Suganesh93 <suganeshrajagopal@gmail.com>
2021-07-22 11:41:47 +0530

    firstscriptjava

---
+++ sysout.java
@@ -0,0 +1,15 @@
+package trailone;
+
+public class sysout {
+	
+private void cost() {
+System.out.println("1000");
+
+}
+public static void main(String[] args) {
+	
+	sysout x = new sysout();
+	x.cost();
+	
+}
+}
